402de5e65d57249f04d82d338868c3da172214af
NoahPodcast <naubert@magesi.com>
2022-09-16 10:50:34 +0200

    *TESTING* Development of the new function (Wings)

--- setup.sh
+++ setup.sh
@@ -109,7 +109,7 @@ sudo systemctl enable --now pteroq.service
 sudo rm /etc/nginx/sites-enabled/default
 sudo bash -c 'cat > /etc/nginx/sites-available/pterodactyl.conf <<EOF
 server {
-    # Replace the example <domain> with your domain name or IP address
+    # Replace the example <localhost> with your domain name or IP address
     listen 80;
     server_name localhost;
 
@@ -157,4 +157,118 @@ server {
 EOF'
 sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
 
+# Restarting System Nginx
 sudo systemctl restart nginx
+
+
+#     Instalation Global (Wings)
+cd
+
+# Installing and starting "Docker" in the user directory
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+sudo systemctl enable --now docker
+
+# Enable docker swap in grub
+sudo rm /etc/default/grub
+sudo bash -c 'cat > /etc/default/grub.txt <<EOF
+# If you change this file, run '"'"'update-grub'"'"' afterwards to update
+# /boot/grub/grub.cfg.
+# For full documentation of the options in this file, see:
+#   info -f grub -n '"'"'Simple configuration'''"'"'
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT_STYLE=hidden
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="swapaccount=1"
+GRUB_CMDLINE_LINUX=""
+
+# Uncomment to enable BadRAM filtering, modify to suit your needs
+# This works with Linux (no patch required) and with any kernel that obtains
+# the memory map information from GRUB (GNU Mach, kernel of FreeBSD ...)
+#GRUB_BADRAM="0x01234567,0xfefefefe,0x89abcdef,0xefefefef"
+
+# Uncomment to disable graphical terminal (grub-pc only)
+#GRUB_TERMINAL=console
+
+# The resolution used on graphical terminal
+# note that you can use only modes which your graphic card supports via VBE
+# you can see them in real GRUB with the command "vbeinfo"
+#GRUB_GFXMODE=640x480
+
+# Uncomment if you dont want GRUB to pass "root=UUID=xxx" parameter to Linux
+#GRUB_DISABLE_LINUX_UUID=true
+
+# Uncomment to disable generation of recovery mode menu entries
+#GRUB_DISABLE_RECOVERY="true"
+
+# Uncomment to get a beep at grub start
+#GRUB_INIT_TUNE="480 440 1"
+EOF'
+
+#   Installing Wings
+
+# Set repositories
+sudo mkdir -p /etc/pterodactyl
+
+# Donwload Wings
+sudo curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+
+# Creation of the inter-file link (Wings)
+sudo chmod u+x /usr/local/bin/wings
+
+# Creation of configuration files
+sudo bash -c 'cat > /etc/pterodactyl/config.yml <<EOF
+debug: false
+uuid: 83374c67-681a-41b5-8598-d1aeb009b0ad
+token_id: x35UzI1SHCa9AZmp
+token: jt70SD7sfwywiLm9QiDGTEzucCkplACIixkQaL9Yruffd3nzf04nG9xPSSa7KJBF
+api:
+  host: 0.0.0.0
+  port: 8080
+  ssl:
+    enabled: false
+    cert: /etc/letsencrypt/live/192.168.2.32/fullchain.pem
+    key: /etc/letsencrypt/live/192.168.2.32/privkey.pem
+  upload_limit: 100
+system:
+  data: /var/lib/pterodactyl/volumes
+  sftp:
+    bind_port: 2022
+allowed_mounts: []
+remote: '"'"'http://192.168.2.32:8080'"'"'
+EOF'
+
+# Starting Wings
+sudo wings --debug
+
+# Running Wings in the background
+sudo bash -c 'cat > /etc/systemd/system/wings.service <<EOF
+[Unit]
+Description=Pterodactyl Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
+
+[Service]
+User=root
+WorkingDirectory=/etc/pterodactyl
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# Activating Wings
+sudo systemctl enable --now wings
+
+# Optional AF2 Activation
+echo | sudo mysql -u root -p << EOF
+UPDATE panel.settings SET value = 0 WHERE `key` = 'settings::pterodactyl:auth:2fa_required';
+EOF'
